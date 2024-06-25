0a28ae30d6f0821050597ce3c71186ef80a9d908
tluyben <tycho@e-lab.nl>
2024-06-25 09:55:23 +0200

    error catching for long running processes

--- index.js
+++ index.js
@@ -27,66 +27,71 @@ const server = new SMTPServer({
   cert: cer ? fs.readFileSync(cer) : undefined,
   onData(stream, session, callback) {
     simpleParser(stream, async (err, parsed) => {
-      if (err) {
-        console.error(err);
-        callback(err);
-        return;
+      try {
+        if (err) {
+          console.error(err);
+          callback(err);
+          return;
+        }
+
+        //   console.log("parsed", parsed, session);
+
+        if (parsed.attachments.length > 0) {
+          // iterate over the attachments, write them in a /tmp random file and replace the attachment.filename with the real actual pathp;
+          parsed.attachments.forEach((attachment) => {
+            const filepath = path.join(
+              "/tmp",
+              Math.random().toString(36).substring(2)
+            );
+            fs.writeFileSync(filepath, attachment.content);
+            attachment.filepath = filepath;
+          });
+        }
+
+        // Construct the JSON object from the parsed email
+        const emailData = {
+          name: parsed.from.value.map((sender) => sender.name).join(", "),
+          from: parsed.from.value.map((from) => from.address).join(", "),
+          to: parsed.to.value.map((to) => to.address).join(", "),
+          subject: parsed.subject,
+          text: parsed.text,
+          html: parsed.html,
+          attachments: parsed.attachments.map((attachment) => ({
+            filepath: attachment.filepath,
+            filename: attachment.filename,
+          })),
+        };
+
+        if (parsed.text && parsed.html) {
+          delete emailData.text;
+        } else if (parsed.text) {
+          delete emailData.html;
+        }
+
+        const fullObj = { user: session.user, email: emailData };
+
+        if (pipeProgram) {
+          // write the fullObj to a random file in /tmp and pass the filename to the shell program
+          const rf = `/tmp/${Math.random().toString(36).substring(2)}.json`;
+          fs.writeFileSync(rf, JSON.stringify(fullObj, null, 2));
+          const childProcess = require("child_process");
+          // console.log("Executing the program", pipeProgram, rf);
+          const child = childProcess.spawn(pipeProgram, [rf]);
+          child.on("error", (err) => {
+            console.error("Error executing the program", err);
+          });
+          child.stdout.on("data", (data) => {
+            console.log(`${data}`);
+          });
+        } else {
+          console.log(JSON.stringify(fullObj, null, 2));
+        }
+
+        callback();
+      } catch (e) {
+        console.error(e);
+        callback(e);
       }
-
-      //   console.log("parsed", parsed, session);
-
-      if (parsed.attachments.length > 0) {
-        // iterate over the attachments, write them in a /tmp random file and replace the attachment.filename with the real actual pathp;
-        parsed.attachments.forEach((attachment) => {
-          const filepath = path.join(
-            "/tmp",
-            Math.random().toString(36).substring(2)
-          );
-          fs.writeFileSync(filepath, attachment.content);
-          attachment.filepath = filepath;
-        });
-      }
-
-      // Construct the JSON object from the parsed email
-      const emailData = {
-        name: parsed.from.value.map((sender) => sender.name).join(", "),
-        from: parsed.from.value.map((from) => from.address).join(", "),
-        to: parsed.to.value.map((to) => to.address).join(", "),
-        subject: parsed.subject,
-        text: parsed.text,
-        html: parsed.html,
-        attachments: parsed.attachments.map((attachment) => ({
-          filepath: attachment.filepath,
-          filename: attachment.filename,
-        })),
-      };
-
-      if (parsed.text && parsed.html) {
-        delete emailData.text;
-      } else if (parsed.text) {
-        delete emailData.html;
-      }
-
-      const fullObj = { user: session.user, email: emailData };
-
-      if (pipeProgram) {
-        // write the fullObj to a random file in /tmp and pass the filename to the shell program
-        const rf = `/tmp/${Math.random().toString(36).substring(2)}.json`;
-        fs.writeFileSync(rf, JSON.stringify(fullObj, null, 2));
-        const childProcess = require("child_process");
-        // console.log("Executing the program", pipeProgram, rf);
-        const child = childProcess.spawn(pipeProgram, [rf]);
-        child.on("error", (err) => {
-          console.error("Error executing the program", err);
-        });
-        child.stdout.on("data", (data) => {
-          console.log(`${data}`);
-        });
-      } else {
-        console.log(JSON.stringify(fullObj, null, 2));
-      }
-
-      callback();
     });
   },
 
@@ -100,6 +105,10 @@ const server = new SMTPServer({
 const port = options.port || 25;
 // const host = options.host || "127.0.0.1";
 
-server.listen(port, () => {
-  console.log(`SMTP server listening on ${port}`);
-});
+try {
+  server.listen(port, () => {
+    console.log(`SMTP server listening on ${port}`);
+  });
+} catch (e) {
+  console.error(e);
+}
